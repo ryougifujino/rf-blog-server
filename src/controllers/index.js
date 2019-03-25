8619cc8dc0503cce40a3a5ce602be930336d37a3
ryougifujino <ryougifujino@gmail.com>
2019-03-25 16:40:52 +0800

    Add replies controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const posts = require('./posts-controller');
 const albums = require('./albums-controller');
 const comments = require('./comments-controller');
+const replies = require('./replies-controller');
 
 module.exports = {
     posts,
     albums,
-    comments
+    comments,
+    replies
 };
