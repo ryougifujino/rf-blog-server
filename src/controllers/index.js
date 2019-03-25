44905aa2d254b25ccab7fa77cbb087006ff7b82c
ryougifujino <ryougifujino@gmail.com>
2019-03-25 20:53:44 +0800

    Add shares controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const posts = require('./posts-controller');
 const albums = require('./albums-controller');
 const comments = require('./comments-controller');
 const replies = require('./replies-controller');
+const shares = require('./shares-controller');
 
 module.exports = {
     posts,
     albums,
     comments,
-    replies
+    replies,
+    shares
 };
