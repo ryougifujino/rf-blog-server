a172f98cdad1c7f506a2ea99512264aa4133fd6f
ryougifujino <ryougifujino@gmail.com>
2019-03-25 15:09:34 +0800

    Add albums router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = new Router();
 const api = new Router();
 
 const posts = require('./posts-router');
+const albums = require('./albums-router');
 
 api.use(posts);
+api.use(albums);
 
 router.use('/api', api.routes());
 
