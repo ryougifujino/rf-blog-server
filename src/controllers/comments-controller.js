3c0630cb524fef3a0ca4603fe2a2341e7959369d
ryougifujino <ryougifujino@gmail.com>
2019-03-26 09:36:29 +0800

    Fix comments validation

--- src/controllers/comments-controller.js
+++ src/controllers/comments-controller.js
@@ -3,22 +3,27 @@ const {ErrorMessages, Pagination} = require('../data/body-templates');
 const DateUtils = require('../lib/date-utils');
 
 const CONTENT_LENGTH_LIMIT = 5000;
+const FROM_USER_LENGTH_LIMIT = 20;
 
 const post = async ctx => {
-    //TODO to validate from_user length
     const {comment: {content, from_user, post_id} = {}} = ctx.request.body;
     if (content && content.length > CONTENT_LENGTH_LIMIT) {
         ctx.status = 400;
         ctx.body = new ErrorMessages("params error", ['content length exceeds limitation']);
         return;
     }
+    if (from_user && from_user.length > FROM_USER_LENGTH_LIMIT) {
+        ctx.status = 400;
+        ctx.body = new ErrorMessages("params error", ['from user length exceeds limitation']);
+        return;
+    }
+    const post = await Post.find(post_id);
+    if (!post) {
+        ctx.status = 400;
+        ctx.body = new ErrorMessages("params error", ['post id does not exist']);
+        return;
+    }
     try {
-        const post = await Post.find(post_id);
-        if (!post) {
-            ctx.status = 400;
-            ctx.body = new ErrorMessages("params error", ['post id does not exist']);
-            return;
-        }
         const comment = await Comment.create({
             content,
             from_user,
