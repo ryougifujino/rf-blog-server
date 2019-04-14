a2b0425eb86a185986cca253f7f552c6671ce1d1
ryougifujino <ryougifujino@gmail.com>
2019-04-15 02:50:08 +0800

    Fix uniqueness of share-categories-post

--- src/controllers/share-categories-controller.js
+++ src/controllers/share-categories-controller.js
@@ -5,12 +5,25 @@ const DateUtils = require('../lib/date-utils');
 const NAME_LENGTH_LIMIT = 100;
 
 const post = async ctx => {
-    const {share_category: {name} = {}} = ctx.request.body;
-    if (name && name.length > NAME_LENGTH_LIMIT) {
+    let {share_category: {name} = {}} = ctx.request.body;
+
+    if (typeof name !== 'string') {
         ctx.status = 400;
-        ctx.body = new ErrorMessages("params error", ['name length exceeds limitation']);
+        ctx.body = new ErrorMessages("params error", ['wrong type of name']);
         return;
     }
+    name = name.trim();
+    if (!name || name.length > NAME_LENGTH_LIMIT) {
+        ctx.status = 400;
+        ctx.body = new ErrorMessages("params error", ['wrong length of name']);
+        return;
+    }
+    if ((await ShareCategory.where({name})).length !== 0) {
+        ctx.status = 409;
+        ctx.body = new ErrorMessages("params error", ['name already exists']);
+        return;
+    }
+
     try {
         const shareCategory = await ShareCategory.create({
             name,
